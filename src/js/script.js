78f21fd024386b287e0e24e25cf2fe5af67feff4
Danil <griss228@gmail.com>
2020-02-05 03:09:25 +0200

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+
+              }
+          ]
+      });
+  });
+      

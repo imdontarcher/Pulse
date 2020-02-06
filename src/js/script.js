48637043ba9739121d64637563ad3b8ad9bc15ec
Danil <griss228@gmail.com>
2020-02-06 02:54:31 +0200

    add no final tabs

--- src/js/script.js
+++ src/js/script.js
@@ -10,9 +10,29 @@ $(document).ready(function(){
               settings: {
                 dots: true,
                 arrows: false
-
               }
+            }
           ]
+      })
+
+      $('ul.catalog_tabs').on('click', 'li:not(.catalog_tab_active)', function() {
+        $(this)
+          .addClass('catalog_tab_active').siblings().removeClass('catalog_tab_active')
+          .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
       });
+
+      function toggleSlide(item){
+        $(item).each(function(i) {
+          $(this).on("click", function(e) {
+            e.preventDefault();
+            $(".catalog-item_content").eq(i).toggleClass("catalog-item_content_active");
+            $(".catalog-item_list").eq(i).toggleClass("catalog-item_list_active");
+          })
+        }) 
+      }
+
+      toggleSlide(".catalog-item_link");
+      toggleSlide(".catalog-item__back");
+
   });
       
